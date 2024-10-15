337790fc604d4d45626cdd6f1bbb9802496e7704
Marcia-tech <marcia.martina@gmail.com>
2024-10-15 11:49:22 -0300

    Create app01.py

---
+++ app01.py
@@ -0,0 +1,37 @@
+# Crie uma variável
+
+'''''
+Esse comentário pode
+ter muitas
+e muitas
+linhas
+'''
+
+'''
+Esse comentário também
+pode ter muitas
+e muitas
+linhas
+'''
+
+
+#Crie uma variável usando "snake case"
+cor_da_casa = "azul"
+
+# Crie uma variável usando "CamelCase"
+corDaCasa = "amarela"
+
+#Exibe as coisas
+print("\n\n\n")
+print(cor_da_casa)
+
+# Primeiro nome
+first_name = "Marcia"
+
+#ùltimo nome
+last_name = "Azeredo"
+
+# Imprimir meu nome
+print(first_name)
+print(last_name)
+
